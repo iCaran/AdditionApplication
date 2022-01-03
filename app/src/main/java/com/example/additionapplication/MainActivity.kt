fa03372c6ae2466c3e2aee6d97f60e1e74d7599c
icaran <kakoburaman@protonmail.com>
2022-01-03 21:38:16 +0530

    v 0.2

--- app/src/main/java/com/example/additionapplication/MainActivity.kt
+++ app/src/main/java/com/example/additionapplication/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.additionapplication
 
+import android.annotation.SuppressLint
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar()?.hide(); //hide the title bar
+
+         */
+        supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        supportActionBar?.setCustomView(R.layout.action_bar_layout)
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
         setContentView(R.layout.activity_main)
 
         val num1=findViewById<EditText>(R.id.num1)
@@ -20,7 +35,7 @@ class MainActivity : AppCompatActivity() {
             val val1=num1.text.toString().toInt()
             val val2=num2.text.toString().toInt()
             val res=val1+val2;
-            result.setText(res.toString())
+            result.setText("Sum of "+val1+" and "+val2+" is "+res.toString())
         }
 
     }
